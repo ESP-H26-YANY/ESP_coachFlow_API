1b7c1152b65a1fbc9159b30beb30343841fc4a73
yyyany <2332491@cegeprdl.ca>
2026-02-10 08:38:24 -0500

    fisrt

---
+++ CoachFlowApi.Api/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models; 
+using System.Text;
+using System.IdentityModel.Tokens.Jwt; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!)),
+            
+            NameClaimType = "sub", 
+            RoleClaimType = "role",
+            ClockSkew = TimeSpan.Zero 
+        };
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication(); 
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
